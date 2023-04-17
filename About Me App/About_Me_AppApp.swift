21a572d7b79e1cb98dfd44001283acea72767b1b
Lia Manning <liapantusa@gmail.com>
2023-04-17 10:38:54 -0400

    Initial Commit

---
+++ About Me App/About_Me_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_AppApp.swift
+//  About Me App
+//
+//  Created by Lia Manning on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
